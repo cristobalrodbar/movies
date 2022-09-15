8a158a703da26db2466dad125e0cc4ec6bd9fca6
Cristóbal Rodríguez <cristobalrodbar@gmail.com>
2022-09-14 22:31:53 -0600

    120

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -10,12 +10,12 @@ class CardSwiper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    if(this.movies.length == 0){
+    if (this.movies.length == 0) {
       return Container(
         width: double.infinity,
-        height: size.height*0.5,
+        height: size.height * 0.5,
         child: Center(child: CircularProgressIndicator()),
-      )
+      );
     }
 
     return Container(
